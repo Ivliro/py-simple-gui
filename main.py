df908f97d5847e716e2e7915b879c85c09f951d3
Miro Krotky <miro.krotky@gmail.com>
2023-03-16 08:41:18 +0100

    create Patient class and make 3 examples

---
+++ main.py
@@ -0,0 +1,9 @@
+from patient import Patient
+from datetime import datetime
+
+
+patients = [
+    Patient("Miro","Krotky",datetime(1990,1,6),185,90.2,True),
+    Patient("Janda","Kovacova",datetime(1995,7,10),165,60.2,False),
+    Patient("Nimish","Narang",datetime(2000,12,12),191,80.2,True),
+]
---
+++ patient.py
@@ -0,0 +1,16 @@
+
+class Patient():
+    
+    #dob = date of birth
+    def __init__(self,first_name,last_name,dob,height,weight,is_medicated):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+        self.height = height
+        self.weight = weight
+        self.is_medicated = is_medicated
+
+    
+    def convert_to_list(self):
+        pass
+        
