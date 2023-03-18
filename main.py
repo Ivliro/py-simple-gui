53940801afacc471816d66039713c0f673afa03f
Miro Krotky <miro.krotky@gmail.com>
2023-03-18 19:21:10 +0100

    used PySimpleGUI, created simple table of data

--- main.py
+++ main.py
@@ -8,9 +8,26 @@ patients = [
     Patient("Nimish","Narang",datetime(2000,12,12),191,80.2,True),
 ]
 
+table_headings = ["first name","last name","dob","height","weight","medicated?"]
+
 def convert_to_rows():
-    new_list = []
+    all_patients = []
     for patient in patients:
-        new_list.append(patient.convert_to_list())
-    return new_list
+        all_patients.append(patient.convert_to_list())
+    return all_patients
+
+#1
+layout = [
+    [sg.Table(headings=table_headings, values=convert_to_rows())]
+]
+#2
+window = sg.Window('Patients\' list', layout)
+
+#3
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+#4
+window.close()
 
